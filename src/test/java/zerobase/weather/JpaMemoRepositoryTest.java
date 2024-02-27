581c14dfd9a031662f246766f65697bdae706715
JaeJun <yyggb12@kakao.com>
2024-02-27 18:08:45 +0900

    feat: 날씨 데이터 저장 기능 추가

---
+++ src/test/java/zerobase/weather/JpaMemoRepositoryTest.java
@@ -0,0 +1,46 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+import zerobase.weather.repository.JpaMemoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest
+@Transactional
+public class JpaMemoRepositoryTest {
+
+    @Autowired
+    JpaMemoRepository jpaMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+        //given
+        Memo newMemo = new Memo(10, "memo1");
+        //when
+        jpaMemoRepository.save(newMemo);
+
+        //then
+        List<Memo> memoList = jpaMemoRepository.findAll();
+        assertFalse(memoList.isEmpty());
+
+    }
+
+    @Test
+    void findByIdTest() {
+        //given
+        Memo newMemo = new Memo(11, "aaa");
+        //when
+        Memo memo = jpaMemoRepository.save(newMemo);
+        //then
+        Optional<Memo> result = jpaMemoRepository.findById(memo.getId());
+        assertEquals("aaa", result.get().getText());
+    }
+}
