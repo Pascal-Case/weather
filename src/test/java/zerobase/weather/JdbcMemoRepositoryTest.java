3873c93d79639c6aecfed4db4939f8d9368fbab0
JaeJun <yyggb12@kakao.com>
2024-03-01 15:51:51 +0900

    refactoring: 사용하지 않는 도메인, 컨트롤러 삭제

--- src/test/java/zerobase/weather/JdbcMemoRepositoryTest.java
+++ src/test/java/zerobase/weather/JdbcMemoRepositoryTest.java
@@ -1,44 +1,34 @@
 package zerobase.weather;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import zerobase.weather.domain.Memo;
-import zerobase.weather.repository.JdbcMemoRepository;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @Transactional
 public class JdbcMemoRepositoryTest {
 
-    @Autowired
-    JdbcMemoRepository jdbcMemoRepository;
-
-    @Test
-    void insertMemoTest() {
-        //given
-        Memo newMemo = new Memo(2, "this is new memo");
-
-        //when
-        jdbcMemoRepository.save(newMemo);
-
-        //then
-        Optional<Memo> result = jdbcMemoRepository.findById(2);
-        assertEquals("this is new memo", result.get().getText());
-
-    }
-
-    @Test
-    void findAllMemoTest() {
-        List<Memo> memoList = jdbcMemoRepository.findAll();
-        System.out.println(memoList);
-        assertNotNull(memoList);
-    }
+//    @Autowired
+//    JdbcMemoRepository jdbcMemoRepository;
+//
+//    @Test
+//    void insertMemoTest() {
+//        //given
+//        Memo newMemo = new Memo(2, "this is new memo");
+//
+//        //when
+//        jdbcMemoRepository.save(newMemo);
+//
+//        //then
+//        Optional<Memo> result = jdbcMemoRepository.findById(2);
+//        assertEquals("this is new memo", result.get().getText());
+//
+//    }
+//
+//    @Test
+//    void findAllMemoTest() {
+//        List<Memo> memoList = jdbcMemoRepository.findAll();
+//        System.out.println(memoList);
+//        assertNotNull(memoList);
+//    }
 
 }
